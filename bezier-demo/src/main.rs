77ea5f59430a286ac8646e61d425194fcbff9e67
ByteTrail <brandon.grooters@bytetrail.com>
2022-08-09 17:32:56 -0500

    initial add of demo app

---
+++ bezier-demo/src/main.rs
@@ -0,0 +1,26 @@
+use clap::{Parser, ArgEnum};
+
+#[derive(ArgEnum, Debug, Clone)]
+enum Feature {
+    PavedRoad,
+    River,
+    Railroad,
+}
+
+#[derive(Parser, Debug)]
+struct Args {
+    /// Initial feature type to display
+    #[clap(short, long, value_parser)]
+    feature: Option<Feature>,
+    /// The number of connected bezier segments to start with
+    #[clap(short, long, value_parser)] 
+    segments: Option<u8>,
+}
+
+fn main() -> windows::core::Result<()> {
+    let _args = Args::parse();
+
+    Ok(())
+}
+
+
